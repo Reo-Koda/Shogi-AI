db0d33ac19858fbfe98010699807627e9244ab40
Reo-Koda <matoryoshika719@gmail.com>
2026-03-02 22:56:11 +0900

    保存するパラメータファイルにモデルの名前を添付するように変更

--- save_pram.py
+++ save_pram.py
@@ -4,7 +4,7 @@ from torch import torch
 def save_pram(model, optimizer, scheduler, epoch, val_loss, savePath="default"):
     if savePath == "default":
         dt_now = datetime.now()
-        savePath = f"./pram/ckpt-{dt_now.strftime('%Y%m%d-%H%M%S')}-{int(val_loss * 1e6)}.pt"
+        savePath = f"./pram/ckpt-{dt_now.strftime('%Y%m%d-%H%M%S')}-{model.__class__.__name__}-{int(val_loss * 1e6)}.pt"
 
     # モデルの保存
     ckpt = {
